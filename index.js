212f4cdd20e48efc884c6484e5e990ec192f9d12
Egil Skjelbred <eskjelbred@gmail.com>
2023-10-21 10:41:07 +0200

    added fill, reset

--- index.js
+++ index.js
@@ -14,75 +14,63 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-db.run(`
-    CREATE TABLE IF NOT EXISTS locations (
-        id INTEGER PRIMARY KEY,
-        city TEXT NOT NULL,
-        country TEXT NOT NULL
-    )
-`, (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log("locations-tabellen opprettet");
-});
-
-db.run(`
-    CREATE TABLE IF NOT EXISTS people (
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        alias TEXT,
-        power TEXT,
-        image TEXT,
-        age INTEGER,
-        location_id INTEGER,
-        FOREIGN KEY (location_id) REFERENCES locations(id)
-    )
-`, (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log("people-tabellen opprettet");
-});
-
-// Fyll locations-tabellen først
-const locations = [
-    { city: "Metropolis", country: "USA" },
-    { city: "Gotham", country: "USA" },
-    { city: "Themyscira", country: "Ukjent" },
-    { city: "Asgard", country: "Ukjent" },
-    { city: "New York", country: "USA" }
-];
-
-locations.forEach(location => {
-    db.run(`INSERT INTO locations (city, country) VALUES (?, ?)`, [location.city, location.country], function (err) {
+// Endepunkt for å tømme hele databasen
+app.delete('/clear-database', (req, res) => {
+    db.run(`DELETE FROM people`, (err) => {
         if (err) {
-            console.error(err.message);
-            return;
+            return res.status(500).json({ error: err.message });
         }
-        const locationId = this.lastID;
 
-        // Basert på locationId, legg til superhelter i people-tabellen
-        switch (location.city) {
-            case "Metropolis":
-                db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, ["Clark Kent", "Superman", "Super strength, flight", "superman_image_url", 35, locationId]);
-                break;
-            case "Gotham":
-                db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, ["Bruce Wayne", "Batman", "Genius intellect, peak human physical condition", "batman_image_url", 40, locationId]);
-                break;
-            case "Themyscira":
-                db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, ["Diana Prince", "Wonder Woman", "Super strength, agility, lasso of truth", "wonderwoman_image_url", 5000, locationId]);
-                break;
-            case "Asgard":
-                db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, ["Thor Odinson", "Thor", "God of thunder, Mjölnir", "thor_image_url", 1500, locationId]);
-                break;
-            case "New York":
-                db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, ["Peter Parker", "Spider-Man", "Wall-crawling, spider sense", "spiderman_image_url", 22, locationId]);
-                break;
-        }
+        db.run(`DELETE FROM locations`, (err) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+
+            res.json({ message: "Databasen er tømt" });
+        });
     });
 });
+
+
+app.post('/fill-database', (req, res) => {
+    const locations = [
+        { city: "Metropolis", country: "USA" },
+        { city: "Gotham", country: "USA" },
+        { city: "Themyscira", country: "Ukjent" },
+        { city: "Asgard", country: "Ukjent" },
+        { city: "New York", country: "USA" }
+    ];
+
+    locations.forEach(location => {
+        db.run(`INSERT INTO locations (city, country) VALUES (?, ?)`, [location.city, location.country], function (err) {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            const locationId = this.lastID;
+
+            switch (location.city) {
+                case "Metropolis":
+                    db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, ["Clark Kent", "Superman", "Super strength, flight", "https://upload.wikimedia.org/wikipedia/en/3/35/Supermanflying.png", 35, locationId]);
+                    break;
+                case "Gotham":
+                    db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, ["Bruce Wayne", "Batman", "Genius intellect, peak human physical condition", "https://i.pinimg.com/originals/35/5c/94/355c94e0f3eea5e63f1862ed1e4366be.png", 40, locationId]);
+                    break;
+                case "Themyscira":
+                    db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, ["Diana Prince", "Wonder Woman", "Super strength, agility, lasso of truth", "https://upload.wikimedia.org/wikipedia/en/thumb/f/fd/Wonder_Woman_DC_Comics.png/220px-Wonder_Woman_DC_Comics.png", 5000, locationId]);
+                    break;
+                case "Asgard":
+                    db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, ["Thor Odinson", "Thor", "God of thunder, Mjölnir", "https://p7.hiclipart.com/preview/759/233/109/thor-cartoon-marvel-cinematic-universe-marvel-animation-comics-thor.jpg", 1500, locationId]);
+                    break;
+                case "New York":
+                    db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, ["Peter Parker", "Spider-Man", "Wall-crawling, spider sense", "https://i.pinimg.com/originals/17/02/d7/1702d7516cfda3b2c1e205a1ef1c1b4f.png", 22, locationId]);
+                    break;
+            }
+        });
+    });
+
+    res.json({ message: "Databasen er fylt" });
+});
+
 
 
 
@@ -116,45 +104,86 @@ app.get('/heroes/:id', (req, res) => {
 
 
 app.post('/add-hero', (req, res) => {
-    const { name, alias, power, image, age, city, country } = req.body;
+    const { name, alias, power, image, age, city, country, location_id } = req.body;
 
-    // Først, sjekk om byen allerede eksisterer i 'locations'-tabellen
-    db.get(`SELECT id FROM locations WHERE city = ? AND country = ?`, [city, country], (err, row) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-
-        let locationId;
-        if (row) {
-            locationId = row.id;
-        } else {
-            // Hvis byen ikke eksisterer, legg den til i 'locations'-tabellen
-            db.run(`INSERT INTO locations (city, country) VALUES (?, ?)`, [city, country], function (err) {
-                if (err) {
-                    res.status(500).json({ error: err.message });
-                    return;
-                }
-                locationId = this.lastID;
-            });
-        }
-
-        // Legg til personen i 'people'-tabellen med den hentede eller nyopprettede 'location_id'
-        db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, [name, alias, power, image, age, locationId], (err) => {
+    if (location_id) {
+        // Hvis location_id er gitt, legg direkte til personen
+        db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, [name, alias, power, image, age, location_id], (err) => {
             if (err) {
                 res.status(500).json({ error: err.message });
                 return;
             }
             res.json({ message: "Person lagt til!" });
         });
-    });
+    } else {
+        // Ellers sjekk om byen allerede eksisterer og legg til hvis nødvendig
+        db.get(`SELECT id FROM locations WHERE city = ? AND country = ?`, [city, country], (err, row) => {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }
+
+            let locationId;
+            if (row) {
+                locationId = row.id;
+            } else {
+                db.run(`INSERT INTO locations (city, country) VALUES (?, ?)`, [city, country], function (err) {
+                    if (err) {
+                        res.status(500).json({ error: err.message });
+                        return;
+                    }
+                    locationId = this.lastID;
+                });
+            }
+
+            db.run(`INSERT INTO people (name, alias, power, image, age, location_id) VALUES (?, ?, ?, ?, ?, ?)`, [name, alias, power, image, age, locationId], (err) => {
+                if (err) {
+                    res.status(500).json({ error: err.message });
+                    return;
+                }
+                res.json({ message: "Person lagt til!" });
+            });
+        });
+    }
 });
 
 app.patch('/hero/:id', (req, res) => {
     const personId = req.params.id;
     const { name, alias, power, image, age } = req.body;
 
-    db.run(`UPDATE people SET name = ?, alias = ?, power = ?, image = ?, age = ? WHERE id = ?`, [name, alias, power, image, age, personId], (err) => {
+    let updateFields = [];
+    let updateValues = [];
+
+    if (name) {
+        updateFields.push("name = ?");
+        updateValues.push(name);
+    }
+    if (alias) {
+        updateFields.push("alias = ?");
+        updateValues.push(alias);
+    }
+    if (power) {
+        updateFields.push("power = ?");
+        updateValues.push(power);
+    }
+    if (image) {
+        updateFields.push("image = ?");
+        updateValues.push(image);
+    }
+    if (age) {
+        updateFields.push("age = ?");
+        updateValues.push(age);
+    }
+
+    if (updateFields.length === 0) {
+        res.status(400).json({ error: "Ingen felter å oppdatere" });
+        return;
+    }
+
+    const sql = `UPDATE people SET ${updateFields.join(", ")} WHERE id = ?`;
+    updateValues.push(personId);
+
+    db.run(sql, updateValues, (err) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
@@ -166,6 +195,7 @@ app.patch('/hero/:id', (req, res) => {
         res.json({ message: "Person oppdatert" });
     });
 });
+
 
 app.delete('/hero/:id', (req, res) => {
     const personId = req.params.id;
